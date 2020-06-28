0370da5df45c1537b7637b6383aba49eac5a9ee4
shinbat <shinbat.daum.net>
2020-06-28 17:23:40 +0900

    리트윗

--- revised/ch5/back/routes/post.js
+++ revised/ch5/back/routes/post.js
@@ -3,7 +3,7 @@ const multer = require('multer');
 const path = require('path');
 const fs = require('fs');
 
-const { Post, Comment, Image, User } = require('../models');
+const { Post, Comment, Image, User, Hashtag } = require('../models');
 const { isLoggedIn } = require('./middlewares');
 
 const router = express.Router();
@@ -31,10 +31,17 @@ const upload = multer({
 
 router.post('/', isLoggedIn, upload.none(), async (req, res, next) => {
     try {
+        const hashtags = req.body.content.match(/#[^\s]+/g);
         const post = await Post.create({
             content: req.body.content,
             UserId: req.user.id,
         });
+        if (hashtags) {
+            const result = await Promise.all(hashtags.map((tag) => Hashtag.findOrCreate({
+                where: { name: tag.slice(1).toLowerCase() },
+            })));   // [[노드, true], [리액트, true]]
+            await post.addHashtags(result.map((v) => v[0]));
+        };
         if (req.body.image) {
             if (Array.isArray(req.body.image)) {    // 이미지를 여러개 올리면 image:[제로초.png, 부기초.png]
       

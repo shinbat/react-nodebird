4fa51d54a9b03e47b4d22544fc14e0dd252337e4
shinbat <shinbat.daum.net>
2020-07-04 07:21:33 +0900

    next bundle

--- revised/ch6/front/next.config.js
+++ revised/ch6/front/next.config.js
@@ -1,15 +1,21 @@
-const { configConsumerProps } = require("antd/lib/config-provider")
-const CompressPlugin = require('compression-webpack-plugin');
+const withBundleAnalyzer = require('@next/bundle-analyzer')({
+    enabled: process.env.ANALYZE === 'true',
+});
 
-module.exports = {
+
+module.exports = withBundleAnalyzer({
+    compress: true,
     webpack(config, { webpack }) {
         const prod = process.env.NODE_ENV === 'production';
-        const plugins = [...config.plugins]
+ 
         return {
             ...config,
             mode: prod ? 'production' : 'development',
             devtool: prod ? 'hidden-source-map' : 'eval',
-            plugins: [...config.plugins],
+            plugins: [
+                ...config.plugins,
+                new webpack.ContextReplacementPlugin(/moment[\/\\]locale$/, /^\.\/ko$/),
+            ],
             module: {
                 ...config.module,
                 rules: [
@@ -17,9 +23,8 @@ module.exports = {
                     {
 
                     }   
-                ]
-            }
-
-        }
-    }
-}
+                ],
+            },
+        };
+    },
+});
